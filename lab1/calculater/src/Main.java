eebad067a21447064f9a33642a4e9ea9b228037b
ahmad-al-nehlawi <ahmad.nhl95@live.com>
2022-10-02 21:20:40 +0300

    lab1 -edited

--- lab1/calculater/src/Main.java
+++ lab1/calculater/src/Main.java
@@ -9,47 +9,47 @@ public class Main {
         String s = input.next();
         System.out.print("Enter number 2: ");
         int snumber = input.nextInt();
-        switch (s){
+        switch (s) {
             case "+":
-                System.out.println( "the result is : " + add(fnumber,snumber));
+                System.out.println("the result is : " + add(fnumber, snumber));
                 break;
             case "-":
-                System.out.println("the result is : " +even(fnumber,snumber));
+                System.out.println("the result is : " + even(fnumber, snumber));
                 break;
             case "*":
-                System.out.println("the result is : " +multy(fnumber,snumber));
+                System.out.println("the result is : " + multy(fnumber, snumber));
                 break;
             case "/":
-
-                div(fnumber,snumber);
-
+                div(fnumber, snumber);
                 break;
             default:
                 System.out.println("Not correct ");
                 System.out.print("Enter a correct operation ( + , - , * , / )");
                 break;
-
         }
+    }
 
+    public static int add(int fnum, int snum) {
+        return fnum + snum;
     }
-    public static int add(int fnum, int snum){
-        return fnum+snum;
-    }
-    public static  int even(int fnum, int snum){
 
-        return fnum-snum;
+    public static int even(int fnum, int snum) {
+
+        return fnum - snum;
     }
-    public static int multy(int fnum, int snum){
-        return fnum*snum;
+
+    public static int multy(int fnum, int snum) {
+        return fnum * snum;
     }
-    public static  int   div(int fnum, int snum) {
+
+    public static int div(int fnum, int snum) {
         int sum = 0;
 
         try {
             sum = fnum / snum;
             return sum;
-        } catch (ArithmeticException ae){
-        System.out.println("the result is : infinity");
+        } catch (ArithmeticException ae) {
+            System.out.println("the result is : infinity");
             return sum;
         }
     }
